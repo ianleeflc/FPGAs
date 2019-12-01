9b957db92d3698c3367774f868a4f84830c33ab3
Li <yli@fortlewis.edu>
2019-11-30 23:19:48 -0700

    update logicBasics.v

--- logicBasics.v
+++ logicBasics.v
@@ -1,17 +1,12 @@
 `timescale 1ns / 1ps
 
-// 2-input AND gate
-module Gates(input a, b, output y);
-    assign y=a&b;
-endmodule
-
-`timescale 1ns / 1ps
-module TestBench();
-    reg a,b;
-    wire y;
-    Gates dut (a,b,y);
-    initial begin
-    a=0;b=0; #10;
-    a=1;b=1; #10;
-    end
+module logicBasics(input a, b, c, d, [1:0] sel,
+                   output y_and, y_or, y_xor, 
+                   y_mux_1, y_mux_2, y_mux_4to1);
+                   
+    assign y_and=a&&b, y_or=a||b, y_xor=a^b; // gates
+    assign y_mux_1=(~sel[0]&&a) || (sel[0]&&b); // 2:1 mux
+    assign y_mux_2=sel[0]?b:a; //sel==1 pick up a, otherwise pick b
+    assign y_mux_4to1=sel[1]?(sel[0]?d:c):(sel[0]?b:a); // 4:1 mux
+    
 endmodule
